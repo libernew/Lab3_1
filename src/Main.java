cca294f66d71d63d49d5bf16a2086d2c4a6a3609
pudgesingle <pudge_single@hotmail.com>
2018-08-31 08:43:59 +0700

    111

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว","0");
+        JOptionPane.showMessageDialog(null,"message","title",JOptionPane.INFORMATION_MESSAGE);
+
+        int ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","ถามเธอ",JOptionPane.YES_NO_OPTION);
+        if(ans==JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"รักนะจุ้บๆ ");
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"จำไว้เลย");
+        }
+    }
+}
